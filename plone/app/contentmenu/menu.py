plone/plone.app.contentmenu

commit c26ab7db5860e4ce70ce12859385538148f68424

Show "add new" menu when there are one or more addable types. Showing a link when only one type was addable caused conflicts with the dropdown JavaScript and generally made for a messy cluster of content actions. Closes #10193
svn path=/plone.app.contentmenu/trunk/; revision=34062

plone/app/contentmenu/menu.py
@@ -396,76 +396,29 @@ class FactoriesSubMenuItem(BrowserSubMenuItem):
 
     submenuId = 'plone_contentmenu_factory'
     order = 30
-
+    title = _(u'label_add_new_item', default=u'Add new\u2026')
+    description = _(u'title_add_new_items_inside_item', default=u'Add new items inside this item')
+    
     def __init__(self, context, request):
         BrowserSubMenuItem.__init__(self, context, request)
         self.context_state = getMultiAdapter((context, request), name='plone_context_state')
 
     @property
     def extra(self):
-        return {'id': 'plone-contentmenu-factories',
-                'hideChildren': self._hideChildren()}
-
-    @property
-    def title(self):
-        itemsToAdd = self._itemsToAdd()
-        showConstrainOptions = self._showConstrainOptions()
-        if showConstrainOptions or len(itemsToAdd) > 1:
-            return _(u'label_add_new_item', default=u'Add new\u2026')
-        elif len(itemsToAdd) == 1:
-            fti=itemsToAdd[0][1]
-            title = fti.Title()
-            if isinstance(title, Message):
-                title = translate(title, context=self.request)
-            else:
-                title = translate(_safe_unicode(title),
-                                  domain='plone',
-                                  context=self.request)
-            return _(u'label_add_type', default='Add ${type}',
-                     mapping={'type' : title})
-        else:
-            return _(u'label_add_new_item', default=u'Add new\u2026')
-
-    @property
-    def description(self):
-        itemsToAdd = self._itemsToAdd()
-        showConstrainOptions = self._showConstrainOptions()
-        if showConstrainOptions or len(itemsToAdd) > 1:
-            return _(u'title_add_new_items_inside_item', default=u'Add new items inside this item')
-        elif len(itemsToAdd) == 1:
-            return itemsToAdd[0][1].Description()
-        else:
-            return _(u'title_add_new_items_inside_item', default=u'Add new items inside this item')
+        return {'id': 'plone-contentmenu-factories'}
 
     @property
     def action(self):
-        addContext = self._addContext()
-        if self._hideChildren():
+        if self._itemsToAdd():
             (addContext, fti) = self._itemsToAdd()[0]
-
             # Ask the types machinery for the add action.
             factories_view = getMultiAdapter((addContext, self.request), name='folder_factories')
             actions = factories_view.addable_types(include=[fti.getId()])
             if actions:
                 return actions[0]["action"]
-
             # Otherwise, fall back on the createObject script
             baseUrl = addContext.absolute_url()
             return '%s/createObject?type_name=%s' % (baseUrl, quote_plus(fti.getId()),)
-        else:
-            return '%s/folder_factories' % self.context_state.folder().absolute_url()
-
-    def icon(self):
-        if self._hideChildren():
-            fti = self._itemsToAdd()[0][1]
-            expr = fti.getIconExprObject()
-            if expr is not None:
-                context = self.context
-                url_tool = getToolByName(context, 'portal_url')
-                portal = url_tool.getPortalObject()
-                ec = createExprContext(aq_parent(context), portal, context)
-                return expr(ec)
-        return None
 
     def available(self):
         itemsToAdd = self._itemsToAdd()
@@ -509,13 +462,6 @@ class FactoriesSubMenuItem(BrowserSubMenuItem):
             return True
         elif len(constrain.getLocallyAllowedTypes()) < len(constrain.getImmediatelyAddableTypes()):
             return True
-
-    @memoize
-    def _hideChildren(self):
-        itemsToAdd = self._itemsToAdd()
-        showConstrainOptions = self._showConstrainOptions()
-        return (len(itemsToAdd) == 1 and not showConstrainOptions)
-
 
 class FactoriesMenu(BrowserMenu):
     implements(IFactoriesMenu)

plone/app/contentmenu/tests.py
@@ -415,9 +415,8 @@ class TestContentMenu(ptc.PloneTestCase):
     def testAddMenuIncluded(self):
         items = self.menu.getMenuItems(self.folder, self.request)
         factoriesMenuItem = [i for i in items if i['extra']['id'] == 'plone-contentmenu-factories'][0]
-        self.assertEqual(factoriesMenuItem['action'], self.folder.absolute_url() + '/folder_factories')
+        self.assertEqual(factoriesMenuItem['action'], self.folder.absolute_url() + '/createObject?type_name=Document')
         self.failUnless(len(factoriesMenuItem['submenu']) > 0)
-        self.assertEqual(factoriesMenuItem['extra']['hideChildren'], False)
 
     def testAddMenuNotIncludedIfNothingToAdd(self):
         self.logout()
@@ -450,18 +449,6 @@ class TestContentMenu(ptc.PloneTestCase):
         factoriesMenuItem = [i for i in items if i['extra']['id'] == 'plone-contentmenu-factories']
         self.failIf(factoriesMenuItem)
 
-    def testAddMenuWithSingleItemCollapses(self):
-        # we need a dummy to test this - should test that if the item does not
-        # support constrain types and there is
-        self.folder.setConstrainTypesMode(1)
-        self.folder.setLocallyAllowedTypes(('Document',))
-        self.folder.setImmediatelyAddableTypes(('Document',))
-        self.folder.manage_permission('Modify constrain types', ('Manager',))
-        items = self.menu.getMenuItems(self.folder, self.request)
-        factoriesMenuItem = [i for i in items if i['extra']['id'] == 'plone-contentmenu-factories'][0]
-        self.assertEqual(factoriesMenuItem['action'], self.folder.absolute_url() + '/createObject?type_name=Document')
-        self.assertEqual(factoriesMenuItem['extra']['hideChildren'], True)
-
     def testAddMenuWithAddViewExpr(self):
         # we need a dummy to test this - should test that if the item does not
         # support constrain types and there is
@@ -473,7 +460,6 @@ class TestContentMenu(ptc.PloneTestCase):
         items = self.menu.getMenuItems(self.folder, self.request)
         factoriesMenuItem = [i for i in items if i['extra']['id'] == 'plone-contentmenu-factories'][0]
         self.assertEqual(factoriesMenuItem['action'], 'custom_expr')
-        self.assertEqual(factoriesMenuItem['extra']['hideChildren'], True)
 
     # Workflow sub-menu
 

plone/app/contentmenu/contentmenu.pt
@@ -20,13 +20,6 @@
                i18n:attributes="title;">
                <span tal:omit-tag="menuItem/action"
                      class="noMenuAction">
-                  <img width="16"
-                       height="16"
-                       alt=""
-                       tal:condition="menuItem/icon"
-                       tal:attributes="src menuItem/icon;
-                                       title menuItem/description"
-                       i18n:attributes="alt;"/>
                   <span tal:content="menuItem/title"
                         i18n:translate="">
                       Title

commit 7769950843e7a5ed1514a284f7568fcae5dedf16

fix regression introduced by r34062 -- now that the factory menu is always a dropdown, the menu itself should always link to folder_factories (for a good fallback without javascript). refs #10193.
svn path=/plone.app.contentmenu/trunk/; revision=34063

plone/app/contentmenu/menu.py
@@ -1,19 +1,14 @@
 from cgi import escape
-from urllib import quote_plus
 
 from plone.memoize.instance import memoize
 from plone.app.content.browser.folderfactories import _allowedTypes
 from plone.app.content.browser.interfaces import IContentsPage
-from zope.i18n import translate
-from zope.i18nmessageid.message import Message
 from zope.interface import implements
 from zope.component import getMultiAdapter, queryMultiAdapter
 from zope.app.publisher.browser.menu import BrowserMenu
 from zope.app.publisher.browser.menu import BrowserSubMenuItem
 
 from Acquisition import aq_base
-from Acquisition import aq_parent
-from Products.CMFCore.Expression import createExprContext
 from Products.CMFCore.utils import getToolByName
 from Products.CMFDynamicViewFTI.interface import ISelectableBrowserDefault
 from Products.CMFPlone import utils
@@ -85,7 +80,6 @@ class ActionsMenu(BrowserMenu):
             return results
 
         actionicons = getToolByName(context, 'portal_actionicons')
-        url_tool = getToolByName(context, 'portal_url')
         portal_url = getToolByName(context, 'portal_url')()
 
         for action in editActions:
@@ -409,16 +403,7 @@ class FactoriesSubMenuItem(BrowserSubMenuItem):
 
     @property
     def action(self):
-        if self._itemsToAdd():
-            (addContext, fti) = self._itemsToAdd()[0]
-            # Ask the types machinery for the add action.
-            factories_view = getMultiAdapter((addContext, self.request), name='folder_factories')
-            actions = factories_view.addable_types(include=[fti.getId()])
-            if actions:
-                return actions[0]["action"]
-            # Otherwise, fall back on the createObject script
-            baseUrl = addContext.absolute_url()
-            return '%s/createObject?type_name=%s' % (baseUrl, quote_plus(fti.getId()),)
+        return '%s/folder_factories' % self.context_state.folder().absolute_url() 
 
     def available(self):
         itemsToAdd = self._itemsToAdd()

plone/app/contentmenu/tests.py
@@ -415,7 +415,7 @@ class TestContentMenu(ptc.PloneTestCase):
     def testAddMenuIncluded(self):
         items = self.menu.getMenuItems(self.folder, self.request)
         factoriesMenuItem = [i for i in items if i['extra']['id'] == 'plone-contentmenu-factories'][0]
-        self.assertEqual(factoriesMenuItem['action'], self.folder.absolute_url() + '/createObject?type_name=Document')
+        self.assertEqual(factoriesMenuItem['action'], self.folder.absolute_url() + '/folder_factories')
         self.failUnless(len(factoriesMenuItem['submenu']) > 0)
 
     def testAddMenuNotIncludedIfNothingToAdd(self):
@@ -459,7 +459,7 @@ class TestContentMenu(ptc.PloneTestCase):
         self.portal.portal_types['Document']._setPropValue('add_view_expr', 'string:custom_expr')
         items = self.menu.getMenuItems(self.folder, self.request)
         factoriesMenuItem = [i for i in items if i['extra']['id'] == 'plone-contentmenu-factories'][0]
-        self.assertEqual(factoriesMenuItem['action'], 'custom_expr')
+        self.assertEqual(factoriesMenuItem['submenu'][0]['action'], 'custom_expr')
 
     # Workflow sub-menu
 