7ab2f3185c9519cc8e6f636924a69fefa20f057b
Maurits van Rees <maurits@vanrees.org>
2016-10-03 16:49:10 +0200

    Back to development: 2.1.10
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.1.9'
+version = '2.1.10.dev0'
 long_description = open('README.rst').read() + '\n'
 long_description += open('CHANGES.rst').read()
 
