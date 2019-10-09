a4a41d4612339513bd69ddbb91b63e5480b44b80
Markus Hilbert <markus.hilbert@iham.at>
2019-10-09 13:56:34 +0200

    check for permission on browser:menuItem

--- plone/app/contentmenu/displayviewsmenu.py
+++ plone/app/contentmenu/displayviewsmenu.py
@@ -1,6 +1,9 @@
+from AccessControl.SecurityManagement import getSecurityManager
 from plone.app.contentmenu.interfaces import IDisplayViewsMenu
 from zope.browsermenu.menu import BrowserMenu
 from zope.component import getAdapters
+from zope.component import getUtility
+from zope.security.interfaces import IPermission
 from zope.interface import implementer
 
 
@@ -13,11 +16,17 @@ class DisplayViewsMenu(BrowserMenu):
         if action.startswith("++view++"):
             action = action[8:]
 
-        for name, item in getAdapters((context, request), self.getMenuItemType()):
+        sm = getSecurityManager()
+
+        for name, item in getAdapters(
+            (context, request), self.getMenuItemType()
+        ):
             item_action = item.action
             # Normalize menu item action; never uses ++view++
             if item_action.startswith("@@"):
                 item_action = item_action[2:]
 
             if item_action == action:
-                return item
+                permission = getUtility(IPermission, name=item.permission)
+                if sm.checkPermission(permission.title, context):
+                    return item
