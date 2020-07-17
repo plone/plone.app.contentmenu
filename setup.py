8d42e4a905d63ab97a338a85dbc3deaf7e68f806
Maurits van Rees <maurits@vanrees.org>
2020-07-17 16:23:35 +0200

    Back to development: 2.3.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '2.3.2'
+version = '2.3.3.dev0'
 long_description = open('README.rst').read() + '\n'
 long_description += open('CHANGES.rst').read()
 
