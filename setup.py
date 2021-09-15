3f6c1cb7931d6d5eec0481180657f4d1a1e32643
Maurits van Rees <maurits@vanrees.org>
2021-09-15 15:55:37 +0200

    Added Python 3.9 classifier.
    [ci skip]

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     keywords='plone contentmenu menu',
     author='Plone Foundation',
